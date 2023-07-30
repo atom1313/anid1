d7786cac23aae99618f6626f962a11c43452c248
atom1313 <93551704+atom1313@users.noreply.github.com>
2023-07-30 20:53:10 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xe9eb77cadadb1f7594b8dd710e6611534993d661";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
